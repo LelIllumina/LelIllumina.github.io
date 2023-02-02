7fe7ebd6b9a29fe209415afafa32ce1039394fa6
Lel Illumina <dragonbilal938@outlook.com>
2023-02-03 00:44:04 +0500

    feat: add button and burger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 var button = document.getElementById("myButton");
+var burgerMenu = document.querySelector(".burger-menu");
+var menu = document.querySelector(".menu");
 button.addEventListener("click", function () {
     alert("Button was clicked!");
+});
+burgerMenu.addEventListener("click", function () {
+    menu.classList.toggle("open");
 });
--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,5 +1,11 @@
 const button = document.getElementById("myButton")!;
+const burgerMenu = document.querySelector(".burger-menu")!;
+const menu = document.querySelector(".menu")!;
 
 button.addEventListener("click", () => {
   alert("Button was clicked!");
+});
+
+burgerMenu.addEventListener("click", () => {
+  menu.classList.toggle("open");
 });
