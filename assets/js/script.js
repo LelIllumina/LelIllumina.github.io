e09f38a7b4e0f1d0f0e6713854cfb8355564a02e
Lel Illumina <dragonbilal938@outlook.com>
2023-02-03 12:03:54 +0500

    feat: make button change text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 var toggleButton = document.querySelector("#theme-switch");
 var elements = document.querySelectorAll("[class=dark-theme]");
 toggleButton.addEventListener("click", function () {
+    if (toggleButton.textContent === "make me cum") {
+        toggleButton.textContent = "make me uncum";
+    }
+    else {
+        toggleButton.textContent = "make me cum";
+    }
     elements.forEach(function (element) {
         element.classList.toggle("light-theme");
         element.classList.toggle("dark-theme");
--- assets/js/script.ts
+++ assets/js/script.ts
@@ -2,6 +2,11 @@ const toggleButton = document.querySelector("#theme-switch")!;
 const elements = document.querySelectorAll("[class=dark-theme]");
 
 toggleButton.addEventListener("click", () => {
+    if (toggleButton.textContent === "make me cum") {
+        toggleButton.textContent = "make me uncum";
+    } else {
+        toggleButton.textContent = "make me cum";
+    }
 	elements.forEach(element => {
 		element.classList.toggle("light-theme");
 		element.classList.toggle("dark-theme");
