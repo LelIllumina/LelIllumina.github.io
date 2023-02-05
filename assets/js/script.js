743a932f907b3159059c5f2de023d42abfbe0bba
Lel Illumina <dragonbilal938@outlook.com>
2023-02-05 22:00:37 +0500

    feat: cunny

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 var toggleButton = document.querySelector("#theme-switch");
 var elements = document.querySelectorAll("[class=dark-theme]");
-toggleButton.addEventListener("click", function () {
-    if (toggleButton.textContent === "make me cum") {
-        toggleButton.textContent = "make me uncum";
-    }
-    else {
-        toggleButton.textContent = "make me cum";
-    }
-    elements.forEach(function (element) {
-        element.classList.toggle("light-theme");
-        element.classList.toggle("dark-theme");
+if (toggleButton && elements) {
+    toggleButton.addEventListener("click", function () {
+        if (toggleButton.textContent === "make me cum") {
+            toggleButton.textContent = "make me uncum";
+        }
+        else {
+            toggleButton.textContent = "make me cum";
+        }
+        elements.forEach(function (element) {
+            element.classList.toggle("light-theme");
+            element.classList.toggle("dark-theme");
+        });
     });
-});
+}
+var burger = document.querySelector(".burger");
+var navLinks = document.querySelector(".nav-links");
+if (burger && navLinks) {
+    burger.addEventListener("click", function () {
+        navLinks.classList.toggle("open");
+    });
+}
--- assets/js/script.ts
+++ assets/js/script.ts
@@ -1,14 +1,25 @@
-const toggleButton = document.querySelector("#theme-switch")!;
+const toggleButton = document.querySelector("#theme-switch");
 const elements = document.querySelectorAll("[class=dark-theme]");
 
-toggleButton.addEventListener("click", () => {
+if (toggleButton && elements) {
+  toggleButton.addEventListener("click", () => {
     if (toggleButton.textContent === "make me cum") {
         toggleButton.textContent = "make me uncum";
     } else {
         toggleButton.textContent = "make me cum";
     }
-	elements.forEach(element => {
-		element.classList.toggle("light-theme");
-		element.classList.toggle("dark-theme");
-	});
-});
+    elements.forEach(element => {
+      element.classList.toggle("light-theme");
+      element.classList.toggle("dark-theme");
+    });
+  });
+}
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+if (burger && navLinks) {
+  burger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+  });
+}
